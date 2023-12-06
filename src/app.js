9a986d5b11f4183e2cd452c261185e2718a0df4d
Himani Bhatt <Himani.Bhatt@rishabhsoft.com>
2023-12-06 15:33:04 +0530

    Init Commit

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const forecast = require('./utils/forecast')
 // console.log(path.join(__dirname, '../public'))
 
 const app = express()
+const port = process.env.PORT || 3000
 const publicDirectoryPath = (path.join(__dirname, '../public'))
 const viewPaths = (path.join(__dirname, '../templates/views'))
 const partialPaths = (path.join(__dirname, '../templates/partials'))
@@ -99,6 +100,6 @@ app.get('/*', (req,res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is run on port 3000')
+app.listen(port, () => {
+    console.log('Server is run on port ' + port)
 })
